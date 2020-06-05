3a4203eeb167e7f293d060f913feed8af403b648
Antoni Kepinski <a@kepinski.me>
2020-06-05 13:07:23 +0200

    use date-fns for formatting dates, allow setting a custom format

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 // @ts-expect-error
 import * as sherlock from 'sherlockjs';
+import {lightFormat} from 'date-fns';
 
-import formatDate from './utils/format-date';
+interface Options {
+	format?: string;
+}
 
 const allowed = /minutes?|from|last|months?|days?|today|next|weeks?|hours?|pm|am|monday|tuesday|wednesday|thursday|friday|saturday|sunday|night|morning|january|february|march|april|may|june|july|august|september|october|november|december/;
 const banned = / in | to |\+|-|\/|\*/;
 
-export default () => async (expression: string): Promise<string> => {
+export default ({format}: Options = {}) => async (expression: string): Promise<string> => {
 	if (allowed.exec(expression) && !banned.exec(expression)) {
 		const result = sherlock.parse(expression);
 
@@ -14,7 +17,12 @@ export default () => async (expression: string): Promise<string> => {
 			return expression;
 		}
 
-		return formatDate(result.startDate);
+		// Fallback to default format
+		try {
+			return lightFormat(result.startDate, format ?? 'MM/dd/yyyy HH:mm');
+		} catch {
+			return lightFormat(result.startDate, 'MM/dd/yyyy HH:mm');
+		}
 	}
 
 	return expression;
--- test.ts
+++ test.ts
@@ -2,7 +2,6 @@ import test from 'ava';
 import {lightFormat, addDays} from 'date-fns';
 
 import parsifyDatePlugin from './src';
-import formatDate from './src/utils/format-date';
 
 test('general', async t => {
 	const result = await parsifyDatePlugin()('17 days from now');
@@ -12,11 +11,20 @@ test('general', async t => {
 	}
 });
 
-test('formats minutes correctly', t => {
-	const fakeDate = new Date('Wed Apr 01 2020 21:05:44 GMT+0200 (Central European Summer Time)');
-	const result = formatDate(fakeDate);
+test('allows setting a different date format', async t => {
+	const result = await parsifyDatePlugin({format: 'yyyy/MM/dd'})('17 days from now');
 
-	t.is(result, '04/01/2020 21:05');
+	if (result.includes(lightFormat(addDays(new Date(), 17), 'yyyy/MM/dd'))) {
+		t.pass();
+	}
+});
+
+test('if the provided date format is invalid, fallback to default', async t => {
+	const result = await parsifyDatePlugin({format: 'foo'})('17 days from now');
+
+	if (result.includes(lightFormat(addDays(new Date(), 17), 'MM/dd/yyyy'))) {
+		t.pass();
+	}
 });
 
 test('if an error occurs, just output the expression', async t => {
