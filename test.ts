0cd1c4adcafe39c8bed1bcb6bbf0febeed082b76
Antoni Kepinski <a@kepinski.me>
2020-06-05 13:31:22 +0200

    experimental changes

--- test.ts
+++ test.ts
@@ -31,7 +31,5 @@ test('if an error occurs, just output the expression', async t => {
 	t.is(await parsifyDatePlugin()('foo / bar'), 'foo / bar');
 	t.is(await parsifyDatePlugin()('1+2'), '1+2');
 	t.is(await parsifyDatePlugin()('3'), '3');
-	t.is(await parsifyDatePlugin()('10 years to days'), '10 years to days');
 	t.is(await parsifyDatePlugin()('fasdfasfasftodayadffdas'), 'fasdfasfasftodayadffdas');
-	t.is(await parsifyDatePlugin()('20 hours to minutes'), '20 hours to minutes');
 });
