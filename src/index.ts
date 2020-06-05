dbc1d54c1e44864f1107d3572b731ac94238533a
Antoni Kepinski <a@kepinski.me>
2020-06-05 13:31:14 +0200

    optimize regex

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ interface Options {
 }
 
 const allowed = /minutes?|from|last|months?|days?|today|next|weeks?|hours?|pm|am|monday|tuesday|wednesday|thursday|friday|saturday|sunday|night|morning|january|february|march|april|may|june|july|august|september|october|november|december/;
-const banned = / in | to |\+|-|\/|\*/;
+const banned = /[*+-/]/;
 
 export default ({format}: Options = {}) => async (expression: string): Promise<string> => {
 	if (allowed.exec(expression) && !banned.exec(expression)) {
