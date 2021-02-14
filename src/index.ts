910ac2b0438fc7db3ee9d1306adbf9df9cc74a18
Antoni Kepinski <xxczaki@pm.me>
2021-02-14 11:31:04 +0100

    0.4.0

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,25 @@
-// @ts-expect-error
-import * as sherlock from 'sherlockjs';
+import {parseDate} from 'chrono-node';
 import {lightFormat} from 'date-fns';
 
 interface Options {
 	format?: string;
 }
 
-const allowed = /minutes?|from|last|months?|days?|today|next|weeks?|hours?|pm|am|monday|tuesday|wednesday|thursday|friday|saturday|sunday|night|morning|january|february|march|april|may|june|july|august|september|october|november|december/;
-const banned = /[*+-/()]/;
-
 export default ({format}: Options = {}) => async (expression: string): Promise<string> => {
-	if (allowed.exec(expression) && !banned.exec(expression)) {
-		const result = sherlock.parse(expression);
+	const result = parseDate(expression);
 
-		if (!result.startDate) {
-			return expression;
-		}
-
-		// Fallback to default format
-		try {
-			return lightFormat(result.startDate, format ?? 'MM/dd/yyyy HH:mm');
-		} catch {
-			return lightFormat(result.startDate, 'MM/dd/yyyy HH:mm');
-		}
+	if (!result) {
+		return expression;
 	}
 
-	return expression;
+	// Fallback to default format
+	try {
+		if (format) {
+			return lightFormat(result, format);
+		}
+
+		return new Date(result).toLocaleString(undefined, {day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric'});
+	} catch {
+		return new Date(result).toLocaleString(undefined, {day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric'});
+	}
 };
--- test.ts
+++ test.ts
@@ -4,27 +4,21 @@ import {lightFormat, addDays} from 'date-fns';
 import parsifyDatePlugin from './src';
 
 test('general', async t => {
-	const result = await parsifyDatePlugin()('17 days from now');
+    const result = await parsifyDatePlugin()('17 days from now');
 
-	if (result.includes(lightFormat(addDays(new Date(), 17), 'MM/dd/yyyy'))) {
-		t.pass();
-	}
+    t.is(result, new Date(addDays(new Date(), 17)).toLocaleString(undefined, {day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric'}));
 });
 
 test('allows setting a different date format', async t => {
 	const result = await parsifyDatePlugin({format: 'yyyy/MM/dd'})('17 days from now');
 
-	if (result.includes(lightFormat(addDays(new Date(), 17), 'yyyy/MM/dd'))) {
-		t.pass();
-	}
+    t.is(result, lightFormat(addDays(new Date(), 17), 'yyyy/MM/dd'));
 });
 
 test('if the provided date format is invalid, fallback to default', async t => {
 	const result = await parsifyDatePlugin({format: 'foo'})('17 days from now');
 
-	if (result.includes(lightFormat(addDays(new Date(), 17), 'MM/dd/yyyy'))) {
-		t.pass();
-	}
+    t.is(result, new Date(addDays(new Date(), 17)).toLocaleString(undefined, {day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric'}));
 });
 
 test('if an error occurs, just output the expression', async t => {
@@ -32,5 +26,6 @@ test('if an error occurs, just output the expression', async t => {
 	t.is(await parsifyDatePlugin()('round(1 month in days)'), 'round(1 month in days)');
 	t.is(await parsifyDatePlugin()('1+2'), '1+2');
 	t.is(await parsifyDatePlugin()('3'), '3');
-	t.is(await parsifyDatePlugin()('fasdfasfasftodayadffdas'), 'fasdfasfasftodayadffdas');
+    t.is(await parsifyDatePlugin()('fasdfasfasftodayadffdas'), 'fasdfasfasftodayadffdas');
+    t.is(await parsifyDatePlugin()('3 weeks in days'), '3 weeks in days');
 });
